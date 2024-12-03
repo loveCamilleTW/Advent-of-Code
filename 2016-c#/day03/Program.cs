3b8de9f6e8b158e1c1594a6e604b4118fea9f6d3
Peter_Lo <peter_lo@cmoney.com.tw>
2024-12-03 09:15:22 +0800

    feat: solve 2016 day3's

---
+++ 2016-c#/day03/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Problem1();
+        Problem2();
+    }
+
+    static void Problem1()
+    {
+        var instructions = File.ReadAllLines("input.txt");
+        var valid = 0;
+
+        foreach (var instruction in instructions) {
+            var sides = instruction.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Array.Sort(sides);
+            if (sides[0] + sides[1] > sides[2]) {
+                valid++;
+            }
+        }
+        Console.WriteLine(valid);
+    }
+    
+    static void Problem2()
+    {
+        var instructions = File.ReadAllLines("input.txt");
+        var valid = 0;
+        
+        for (var i = 0; i < instructions.Length; i += 3) {
+            var sides = new[] {
+                instructions[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray(),
+                instructions[i + 1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray(),
+                instructions[i + 2].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray(),
+            };
+            
+            for (var j = 0; j < 3; j++) {
+                var temp = new[] { sides[0][j], sides[1][j], sides[2][j] };
+                Array.Sort(temp);
+                if (temp[0] + temp[1] > temp[2]) {
+                    valid++;
+                }
+            }
+        }
+        Console.WriteLine(valid);
+    }
+}
