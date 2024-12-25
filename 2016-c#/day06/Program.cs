eb63f113c649b591fa51566c3ce8e9dcb39bd74d
Peter_Lo <peter_lo@cmoney.com.tw>
2024-12-25 17:02:46 +0800

    feat: solve 2016 day6's

---
+++ 2016-c#/day06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Problem1();
+        Problem2();
+    }
+
+    static void Problem1()
+    {
+        var lines = File.ReadAllLines("input.txt");
+        var message = "";
+
+        for (var i = 0; i < lines[0].Length; i++) {
+            var frequency = lines.Select(line => line[i])
+                .GroupBy(c => c)
+                .OrderByDescending(g => g.Count())
+                .First()
+                .Key;
+            message += frequency;
+        }
+        Console.WriteLine(message);
+    }
+    
+    static void Problem2()
+    {
+        var lines = File.ReadAllLines("input.txt");
+        var message = "";
+
+        for (var i = 0; i < lines[0].Length; i++) {
+            var frequency = lines.Select(line => line[i])
+                .GroupBy(c => c)
+                .OrderBy(g => g.Count())
+                .First()
+                .Key;
+            message += frequency;
+        }
+        Console.WriteLine(message);
+    }
+}
